b70662ce6a2a8cd0f7ae2ce3e90edd1524c13a32
masataka <m@clipcrow.com>
2022-05-17 15:48:34 +0900

    Object -> string

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ import { PushEvent } from '@octokit/webhooks-definitions/schema';
 
 if (github.context.eventName === 'push') {
     const pushPayload = github.context.payload as PushEvent;
-    core.info(`The head commit is: ${pushPayload.head_commit}`);
+    core.info(`The head commit is: ${pushPayload.head_commit?.message}`);
 }
