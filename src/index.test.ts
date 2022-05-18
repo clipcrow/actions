857141c6d8f69facecf5e04023c1a6206bf2c840
masataka <m@clipcrow.com>
2022-05-18 23:43:26 +0900

    add PoC of testing

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,15 @@
-import * as issues from './index';
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+import { handleEvent } from './index';
 
 test('handleEvent', () => {
-    // expect('').toEqual(issues.handleEvent());
-});
+    github.context.eventName = 'issues';
+    github.context.payload = require('./event/issues_opened.json');
+    const spy = jest.spyOn(core, 'info');
+
+    handleEvent();
+
+    expect(spy.mock.calls[0].at(0)).toEqual('opened');
+    spy.mockRestore();
+})
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
-import { IssuesEvent } from '@octokit/webhooks-definitions/schema';
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import { IssuesEvent } from '@octokit/webhooks-definitions/schema'
 
-export function handleEvent(event: IssuesEvent): string {
-    return '';
+export function handleEvent () {
+    if (github.context.eventName === 'issues') {
+        const event = github.context.payload as IssuesEvent;
+        core.info(event.action);
+    }
 }
 
-if (github.context.eventName === 'issues') {
-    const event = github.context.payload as IssuesEvent;
-    core.info(JSON.stringify(event));
-    handleEvent(event);
-}
+handleEvent();
